c92d67f089cabae6f6a56b42d9b560266852364a
Eirik S. Morland <eirik@morland.no>
2015-04-01 14:53:20 +0200

    Make register button and map be different blocks, to be able to show contact block when logged in.

--- modules/custom/camposlo/src/Plugin/Block/ContactBlock.php
+++ modules/custom/camposlo/src/Plugin/Block/ContactBlock.php
@@ -7,13 +7,7 @@
 
 namespace Drupal\camposlo\Plugin\Block;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Routing\UrlGeneratorTrait;
-use Drupal\Core\Url;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Block\BlockBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'ContactBlock' block.
@@ -23,57 +17,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *  admin_label = @Translation("contact_block"),
  * )
  */
-class ContactBlock extends BlockBase implements ContainerFactoryPluginInterface {
-
-  use UrlGeneratorTrait;
-
-  /**
-   * The route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $routeMatch;
-
-  /**
-   * Constructs a new UserLoginBlock instance.
-   *
-   * @param array $configuration
-   *   The plugin configuration, i.e. an array with configuration values keyed
-   *   by configuration option name. The special key 'context' may be used to
-   *   initialize the defined contexts by setting it to an array of context
-   *   values keyed by context names.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
-   *   The route match.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->routeMatch = $route_match;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('current_route_match')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function blockAccess(AccountInterface $account) {
-    $route_name = $this->routeMatch->getRouteName();
-    return ($account->isAnonymous() && !in_array($route_name, array('user.register', 'user.login')));
-  }
+class ContactBlock extends BlockBase {
 
   /**
    * {@inheritdoc}
@@ -81,18 +25,7 @@ class ContactBlock extends BlockBase implements ContainerFactoryPluginInterface 
   public function build() {
 
     return array(
-      '#markup' => '<div class="section" id="contact"><!-- heading -->
-                      <div class="container">
-                      <div class="row text-center">
-                      <div class="section-heading">
-                      <h2 class="title"><span>' . $this->t('Sign up!') . '</span></h2>
-
-                      <p class="subtitle"><a class="btn btn-lg btn-primary" href="/user/register">' . $this->t('Register') . '</a></p>
-                      </div>
-                      </div>
-                      </div>
-                      <!-- End heading --><!-- Contact detail -->
-
+      '#markup' => '<div class="section" id="map-inner"><!-- heading -->
                       <div class="contact-address clearfix bg-parallax2">
                       <div class="container">
                       <div class="row">
